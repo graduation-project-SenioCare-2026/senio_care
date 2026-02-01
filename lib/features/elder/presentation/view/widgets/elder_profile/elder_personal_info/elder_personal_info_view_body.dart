dd84d6795cc62388eb6fe708c802dca047afcea3
Rana Gebril <ranagebril5@gmail.com>
2026-02-01 06:59:53 +0200

    fix info card

--- lib/features/elder/presentation/view/widgets/elder_profile/elder_personal_info/elder_personal_info_view_body.dart
+++ lib/features/elder/presentation/view/widgets/elder_profile/elder_personal_info/elder_personal_info_view_body.dart
@@ -1,13 +1,17 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:senio_care/core/constants/app_icons.dart';
 import 'package:senio_care/core/responsive/size_helper.dart';
 import 'package:senio_care/core/routes/routes_names.dart';
 import 'package:senio_care/core/theme/app_colors.dart';
 import 'package:senio_care/core/theme/font_manager.dart';
 import 'package:senio_care/core/theme/font_style.dart';
+import 'package:senio_care/core/user/profile_manager.dart';
 import 'package:senio_care/features/elder/presentation/view/widgets/elder_profile/elder_personal_info/avatar_name_container.dart';
 import 'package:senio_care/features/elder/presentation/view/widgets/elder_profile/elder_personal_info/info_card.dart';
+import 'package:senio_care/features/elder/presentation/view_model/elder_profile/elder_profile_bloc.dart';
+import 'package:senio_care/features/elder/presentation/view_model/elder_profile/elder_profile_event.dart';
 
 class ElderPersonalInfoViewBody extends StatelessWidget {
   const ElderPersonalInfoViewBody({super.key});
@@ -38,10 +42,20 @@ class ElderPersonalInfoViewBody extends StatelessWidget {
             ),
             actions: [
               GestureDetector(
-                onTap: () => Navigator.pushNamed(
-                  context,
-                  RoutesNames.elderEditProfile,
-                ),
+                onTap: () async {
+                  final wasUpdated = await Navigator.pushNamed(
+                    context,
+                    RoutesNames.elderEditProfile,
+                  );
+
+
+                  if (wasUpdated == true && context.mounted) {
+                    final elderId = ProfileManager().elder?.id;
+                    if (elderId != null) {
+                      context.read<ElderProfileBloc>().add(GetElderEvent(elderId));
+                    }
+                  }
+                },
                 child: Padding(
                   padding: EdgeInsetsDirectional.only(
                     end: context.setWidth(10),
--- lib/features/elder/presentation/view/widgets/elder_profile/elder_personal_info/info_card.dart
+++ lib/features/elder/presentation/view/widgets/elder_profile/elder_personal_info/info_card.dart
@@ -1,100 +1,106 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:senio_care/core/common_widgets/custom_card.dart';
 import 'package:senio_care/core/responsive/size_helper.dart';
-import 'package:senio_care/core/user/profile_manager.dart';
 import 'package:senio_care/features/elder/presentation/view/widgets/elder_profile/elder_personal_info/info_row.dart';
+import 'package:senio_care/features/elder/presentation/view_model/elder_profile/elder_profile_bloc.dart';
+import 'package:senio_care/features/elder/presentation/view_model/elder_profile/elder_profile_state.dart';
 
 class InfoCard extends StatelessWidget {
   const InfoCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final elder = ProfileManager().elder;
+
     return SliverPadding(
-      padding: EdgeInsets.symmetric(
-        horizontal: context.setWidth(25),
-      ),
-      sliver: SliverToBoxAdapter(
-        child: CustomCard(
-          child: Column(
-            children: [
-              InfoRow(
-                label: "elderId".tr(),
-                icon: Icons.person,
-                value: elder?.id,
-                copyEnabled: true,
-                dataToCopy: elder?.id,
-              ),
-              const Divider(),
+      padding: EdgeInsets.symmetric(horizontal: context.setWidth(25)),
+      sliver: BlocBuilder<ElderProfileBloc, ElderProfileState>(
+        buildWhen: (previous, current) =>
+            previous.getElderStatus != current.getElderStatus ||
+            previous.editElderProfileStatus != current.editElderProfileStatus,
+        builder: (context, state) {
+          final elder = state.getElderStatus?.data??state.editElderProfileStatus.data;
+          return SliverToBoxAdapter(
+            child: CustomCard(
+              key: ValueKey(elder?.id),
+              child: Column(
+                children: [
+                  InfoRow(
+                    label: "elderId".tr(),
+                    icon: Icons.person,
+                    value: elder?.id,
+                    copyEnabled: true,
+                    dataToCopy: elder?.id,
+                  ),
+                  const Divider(),
 
-              InfoRow(
-                label: "age".tr(),
-                icon: Icons.cake,
-                value: elder?.age?.toString() ?? "-",
-              ),
-              const Divider(),
+                  InfoRow(
+                    label: "age".tr(),
+                    icon: Icons.cake,
+                    value: elder?.age?.toString() ?? "-",
+                  ),
+                  const Divider(),
 
-              InfoRow(
-                label: "gender".tr(),
-                icon: elder?.gender == 'male'
-                    ? Icons.male
-                    : Icons.female,
-                value: elder?.gender,
-              ),
-              const Divider(),
+                  InfoRow(
+                    label: "gender".tr(),
+                    icon: elder?.gender == 'male' ? Icons.male : Icons.female,
+                    value: elder?.gender,
+                  ),
+                  const Divider(),
 
-              InfoRow(
-                label: "weight".tr(),
-                icon: Icons.monitor_weight,
-                value: elder?.weight?.toString() ?? "-",
-              ),
-              const Divider(),
+                  InfoRow(
+                    label: "weight".tr(),
+                    icon: Icons.monitor_weight,
+                    value: elder?.weight?.toString() ?? "-",
+                  ),
+                  const Divider(),
 
-              InfoRow(
-                label: "height".tr(),
-                icon: Icons.height,
-                value: elder?.height?.toString() ?? "-",
-              ),
-              const Divider(),
+                  InfoRow(
+                    label: "height".tr(),
+                    icon: Icons.height,
+                    value: elder?.height?.toString() ?? "-",
+                  ),
+                  const Divider(),
 
-              InfoRow(
-                label: "chronicDisease".tr(),
-                icon: Icons.monitor_heart,
-                values: elder?.chronicDiseases,
-              ),
-              const Divider(),
+                  InfoRow(
+                    label: "chronicDisease".tr(),
+                    icon: Icons.monitor_heart,
+                    values: elder?.chronicDiseases,
+                  ),
+                  const Divider(),
 
-              InfoRow(
-                label: "allergies".tr(),
-                icon: Icons.coronavirus,
-                values: elder?.allergies,
-              ),
-              const Divider(),
+                  InfoRow(
+                    label: "allergies".tr(),
+                    icon: Icons.coronavirus,
+                    values: elder?.allergies,
+                  ),
+                  const Divider(),
 
-              InfoRow(
-                label: "bloodType".tr(),
-                icon: Icons.bloodtype,
-                value: elder?.bloodType,
-              ),
-              const Divider(),
+                  InfoRow(
+                    label: "bloodType".tr(),
+                    icon: Icons.bloodtype,
+                    value: elder?.bloodType,
+                  ),
+                  const Divider(),
 
-              InfoRow(
-                label: "mobilityStatus".tr(),
-                icon: Icons.wheelchair_pickup_outlined,
-                values: elder?.allergies,
-              ),
-              const Divider(),
+                  InfoRow(
+                    label: "mobilityStatus".tr(),
+                    icon: Icons.wheelchair_pickup_outlined,
+                    value: elder?.mobilityStatus?.trim(),
+                  ),
+                  const Divider(),
 
-
-              InfoRow(
-                label: "caregivers".tr(),
-                icon: Icons.volunteer_activism,
-                values: elder?.caregiverIds,
+                  InfoRow(
+                    label: "caregivers".tr(),
+                    icon: Icons.volunteer_activism,
+                    values: elder?.caregiverIds,
+                  ),
+                ],
               ),
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
     );
   }
